1d3ec2b2239d14fe76f9d35bc984dcd1e8c73eee
azltdanh <az.ltdanh@gmail.com>
2015-02-13 17:33:25 +0700

    Fixed overlap problem & add tag-it library

--- js/collra/api.js
+++ js/collra/api.js
@@ -1,100 +1,9 @@
-function collraApi()
-{
-	var _result = _generateResult(40);
+function collraApi() {
+    // generate 40 items.
+    var _result = _generateResult(40);
 
-	// generate 40 items.
-	function _generateResult(count)
-	{
-		var result = [];
-		for (var i=1; i <= count; i++)
-		{
-			result.push(_generateItem(i));
-		}
-		return result;
-	}
-
-	function _generateNumber(max)
-	{
-		return Math.floor(Math.random() * max) + 1;
-	}
-
-
-	function _generateText(maxlen)
-	{
-		var text = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Ut odio. Nam sed est. Nam a risus et est iaculis adipiscing. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Integer ut justo. In tincidunt viverra nisl. Donec dictum malesuada magna. Curabitur id nibh auctor tellus adipiscing pharetra. Fusce vel justo non orci semper feugiat. Cras eu leo at purus ultrices tristique."; 
-		var len = _generateNumber(maxlen);
-		return text.substring(0, len);
-	}
-
-	function _generateStatistic()
-	{
-		return {
-			'views' : _generateNumber(500000),
-			'likes': _generateNumber(500000),
-			'comments': _generateNumber(500000),
-			'rating' : _generateNumber(5)
-		}
-	}
-
-	function _generateImg()
-	{
-		return '/img/items/' + _generateNumber(40) + '.jpg'
-	}
-
-
-	function _generateUser()
-	{
-		var availableNames =
-		[
-			'Roger',
-			'Ferm',
-			'Leif-Gunnar',
-			'Elofsson',
-			'Björn',
-			'Eklöf',
-			'Maj-Len',
-			'Lyth',
-			'Bjorn',
-			'Herlin',
-			'Jocke', 
-			'Widin',
-			'Guido',
-			'Paul',
-			'Sören', 
-			'Clavebring',
-			'Jörgen',
-			'Höjer',
-			'Ann',
-			'Annsson',
-			'Malin',
-			'Fritzell',
-			'Alexander',
-			'Johansson',
-			'Christer',
-			'Lindhök',
-			'Patrik',
-			'Hanberger',
-			'Christian',
-			'Nordin',
-			'Janne'
-		]
-
-		var user = 
-		{
-			'first_name' : availableNames[_generateNumber(availableNames.length)-1],
-			'last_name' : availableNames[_generateNumber(availableNames.length)-1],
-			'screen_name' : availableNames[_generateNumber(availableNames.length)-1].substring(0, 2) + 
-							availableNames[_generateNumber(availableNames.length)-1].substring(0, 2),
-			'avatar' : '/img/user/user.png'
-		}
-
-		return user
-	}
-
-	function _generateTags()
-	{
-		var availableTags =
-		[
+    function _getAvailableTags() {
+        return [
 			'Music & Movies',
 			'Vehicle',
 			'Antique & Design',
@@ -130,41 +39,121 @@ function collraApi()
 			'Playing Cards',
 			'Advent Calendars',
 			'Marianne Richter'
+        ];
+    }
+
+    function _generateResult(count) {
+        var result = [];
+        for (var i = 1; i <= count; i++) {
+            result.push(_generateItem(i));
+        }
+        return result;
+    }
+
+    function _generateNumber(max) {
+        return Math.floor(Math.random() * max) + 1;
+    }
+
+    function _generateText(maxlen) {
+        var text = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Ut odio. Nam sed est. Nam a risus et est iaculis adipiscing. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Integer ut justo. In tincidunt viverra nisl. Donec dictum malesuada magna. Curabitur id nibh auctor tellus adipiscing pharetra. Fusce vel justo non orci semper feugiat. Cras eu leo at purus ultrices tristique.";
+        var len = _generateNumber(maxlen);
+        return text.substring(0, len);
+    }
+
+    function _generateStatistic() {
+        return {
+            'views': _generateNumber(500000),
+            'likes': _generateNumber(500000),
+            'comments': _generateNumber(500000),
+            'rating': _generateNumber(5)
+        }
+    }
+
+    function _generateImg() {
+        return '/img/items/' + _generateNumber(40) + '.jpg'
+    }
+
+    function _generateUser() {
+        var availableNames =
+		[
+			'Roger',
+			'Ferm',
+			'Leif-Gunnar',
+			'Elofsson',
+			'Björn',
+			'Eklöf',
+			'Maj-Len',
+			'Lyth',
+			'Bjorn',
+			'Herlin',
+			'Jocke',
+			'Widin',
+			'Guido',
+			'Paul',
+			'Sören',
+			'Clavebring',
+			'Jörgen',
+			'Höjer',
+			'Ann',
+			'Annsson',
+			'Malin',
+			'Fritzell',
+			'Alexander',
+			'Johansson',
+			'Christer',
+			'Lindhök',
+			'Patrik',
+			'Hanberger',
+			'Christian',
+			'Nordin',
+			'Janne'
 		]
 
-		var tags = [];
-		for (var i=0; i < _generateNumber(10); i++)
+        var user =
 		{
-			tags.push(availableTags[_generateNumber(availableTags.length)-1]);
+		    'first_name': availableNames[_generateNumber(availableNames.length) - 1],
+		    'last_name': availableNames[_generateNumber(availableNames.length) - 1],
+		    'screen_name': availableNames[_generateNumber(availableNames.length) - 1].substring(0, 2) +
+							availableNames[_generateNumber(availableNames.length) - 1].substring(0, 2),
+		    'avatar': '/img/user/user.png'
 		}
 
-		return tags
-	}
+        return user
+    }
 
-	function _generateItem(id)
-	{
-		var item = {
+    function _generateTags() {
+        var availableTags = _getAvailableTags();
 
-			'id' : id,
-			'item' : {
-				'title' : _generateText(75),
-				'img' : _generateImg(),
-				'description' : _generateText(512),
-			},
-			'user' : _generateUser(),
-			'statistic' : _generateStatistic(),
-			'tags' : _generateTags()
-		};
+        var tags = [];
+        for (var i = 0; i < _generateNumber(10) ; i++) {
+            tags.push(availableTags[_generateNumber(availableTags.length) - 1]);
+        }
 
-		return item;
-	}
+        return tags
+    }
 
-	function _search(words)
-	{
-		return _result;
-	}
+    function _generateItem(id) {
+        var item = {
+            'id': id,
+            'item': {
+                'title': _generateText(75),
+                'img': _generateImg(),
+                'description': _generateText(512),
+            },
+            'user': _generateUser(),
+            'statistic': _generateStatistic(),
+            'tags': _generateTags()
+        };
 
-	return {
-		'search' : _search
-	}
+        return item;
+    }
+
+    function _search(words) {
+        return _result;
+    }
+
+    return {
+        'search': _search,
+        'getAvailableTags': _getAvailableTags
+    }
 }
