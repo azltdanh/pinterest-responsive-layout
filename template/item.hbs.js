70038ccc01400cef6159c78b7e0980988d6a7b83
azltdanh <az.ltdanh@gmail.com>
2015-02-25 16:51:33 +0700

    implement Infinite Ajax Scroll v2.2.0
    TODO: using handlebars for page.html

--- template/item.hbs.js
+++ template/item.hbs.js
@@ -24,12 +24,12 @@
     + escapeExpression(lambda(((stack1 = (depth0 != null ? depth0.statistic : depth0)) != null ? stack1.comments : stack1), depth0))
     + "</span></li>\r\n            </ul>\r\n        </div>\r\n        <hr />\r\n        <div class=\"grid-item-user\">\r\n            <a href=\"#\">\r\n                <img src=\""
     + escapeExpression(lambda(((stack1 = (depth0 != null ? depth0.user : depth0)) != null ? stack1.avatar : stack1), depth0))
-    + "\" alt=\"Az Ltdanh\" class=\"img-circle\" width=\"34\" height=\"34\">\r\n                <span class=\"hidden-sm hidden-md\">"
+    + "\" alt=\"Az Ltdanh\" class=\"img-circle\" width=\"34\" height=\"34\">\r\n                <span>"
     + escapeExpression(lambda(((stack1 = (depth0 != null ? depth0.user : depth0)) != null ? stack1.screen_name : stack1), depth0))
     + "</span>\r\n            </a>\r\n        </div>\r\n    </div>\r\n";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
   var stack1, buffer = "﻿<div id=\"collra_result\" class=\"grid\">\r\n";
   stack1 = helpers.each.call(depth0, depth0, {"name":"each","hash":{},"fn":this.program(1, data),"inverse":this.noop,"data":data});
   if (stack1 != null) { buffer += stack1; }
-  return buffer + "</div>";
+  return buffer + "\r\n    <nav class=\"pager\">\r\n        <a href=\"page.html\" class=\"btn btn-block btn-success\">Next</a>\r\n    </nav>\r\n\r\n</div>";
 },"useData":true});
